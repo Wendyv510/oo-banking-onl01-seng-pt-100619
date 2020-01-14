518f1c332702165708761b1d711eda5b3bd4c8d1
Wendy Harris <wendyv510@hotmail.com>
2020-01-14 00:14:41 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,36 @@
 class BankAccount
+  
+  attr_accessor :balance, :status 
+  
+  attr_reader   :name 
+  
+  @@all = []
+  
+  def initialize(name)
+    @balance = 1000
+    @name = name 
+    @status = "open"
+    @@all << self 
+  end 
+  
+  def self.all 
+    @@all
+  end 
+  
+  def deposit(money_amount)
+    @balance += money_amount 
+  end 
+  
+  def display_balance
+     "Your balance is $#{@balance}."
+  end 
+  
+  def valid?
+    @status == "open" && @balance > 0 ? true : false 
+  end 
+  
+  def close_account 
+    @status = "closed"
+  end 
 
 end
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,55 @@
 class Transfer
-  # your code here
-end
+  
+  attr_accessor  :status, :amount, :sender, :reciever 
+  
+  #attr_reader :sender, :receiver 
+  
+  def initialize(sender,receiver,amount)
+    @sender = sender 
+    @receiver = receiver 
+    @status = "pending"
+    @amount = 50
+  end 
+  
+  def valid?
+    if 
+     @sender.valid? && @receiver.valid?
+      true 
+    else 
+      false 
+    end 
+  end 
+  
+  def sender(name)
+    BankAccount.all.select {|account| account == @sender}
+  end
+  
+  def receiver(name) 
+    BankAccount.all.select {|account| account == @receiver}
+  end 
+    
+  
+  def execute_transaction(sender,receiver,amount)
+    if @sender.balance > @amount  && @status.valid? == true 
+          @sender.balance -= @amount 
+          @receiver.balance += @amount
+          @status = "Complete"
+    elsif @status == "Complete"
+          "Transaction already completed."
+    else 
+          @sender.balance < @amount || @sender.status = "closed"
+          @status = "Rejected"
+          "Transaction rejected. Please check your account balance."
+    end 
+  end 
+  
+  def reverse_transfer 
+    if @status == "complete" 
+       @sender.balance += @amount 
+       @receiver.balance -= @amount 
+       @status = "reversed" 
+    end 
+  end 
+  
+end 
+  
